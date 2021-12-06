b75372246e40291304b06bfdb9a75859a656c230
Guto Leoni <guto-acmilan@hotmail.com>
2021-12-06 14:18:00 -0300

    fixing the link between the SFC requests and links/nodes

--- repository/sfc_request_repository.py
+++ repository/sfc_request_repository.py
@@ -1,6 +1,6 @@
 from data_base_manager import DataBaseManager
 from pymongo.errors import BulkWriteError
-from bson.dbref import DBRef
+# from bson.dbref import DBRef
 
 class SfcRequestRepository():
     def __init__(self) -> None:
@@ -10,17 +10,20 @@ class SfcRequestRepository():
     def insert_sfc_request(self, sfc_request: dict)-> None:
         try:
             for i in range(len(sfc_request['VNFs'])):
-                sfc_request['VNFs'][i]['node_name'] = DBRef("nodes", sfc_request['VNFs'][i]['node_name'])
+                # sfc_request['VNFs'][i]['node_name'] = DBRef("nodes", sfc_request['VNFs'][i]['node_name'])
+                sfc_request['VNFs'][i]['node_name'] = sfc_request['VNFs'][i]['node_name']
 
             for i in range(len(sfc_request['flow_entries'])):
-                sfc_request['flow_entries'][i]['destination'] = DBRef("nodes", sfc_request['flow_entries'][i]['destination'])
-                sfc_request['flow_entries'][i]['source'] = DBRef("nodes", sfc_request['flow_entries'][i]['source'])
+                # sfc_request['flow_entries'][i]['destination'] = DBRef("nodes", sfc_request['flow_entries'][i]['destination'])
+                sfc_request['flow_entries'][i]['destination'] = sfc_request['flow_entries'][i]['destination']
+                
+                # sfc_request['flow_entries'][i]['source'] = DBRef("nodes", sfc_request['flow_entries'][i]['source'])
+                sfc_request['flow_entries'][i]['source'] = sfc_request['flow_entries'][i]['source']
 
                 new_path_list = []
                 for j in range(len(sfc_request['flow_entries'][i]['path'])):
-                    new_path_list.append(
-                                        DBRef("links", sfc_request['flow_entries'][i]['path'][j][0]+'-'+sfc_request['flow_entries'][i]['path'][j][1])
-                                        )
+                    # new_path_list.append(DBRef("links", sfc_request['flow_entries'][i]['path'][j][0]+'-'+sfc_request['flow_entries'][i]['path'][j][1]))
+                    new_path_list.append(sfc_request['flow_entries'][i]['path'][j][0]+'-'+sfc_request['flow_entries'][i]['path'][j][1])
                 
                 sfc_request['flow_entries'][i]['path'] = new_path_list
 
